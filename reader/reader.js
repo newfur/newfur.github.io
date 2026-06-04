9c0297366b5c9e86a02bf61e80e985e5e222f411
fancyburn <fancyburn@protonmail.com>
2026-06-04 21:32:37 +0800

    chore: bump version to v1.3.0 and update README

--- reader/reader.js
+++ reader/reader.js
@@ -848,10 +848,10 @@ function initUIEventBindings() {
           try {
             versionDisplay.textContent = 'v' + chrome.runtime.getManifest().version;
           } catch (e) {
-            versionDisplay.textContent = 'v1.2.2';
+            versionDisplay.textContent = 'v1.3.0';
           }
         } else {
-          versionDisplay.textContent = 'v1.2.2';
+          versionDisplay.textContent = 'v1.3.0';
         }
       }
       aboutDialog.showModal();
