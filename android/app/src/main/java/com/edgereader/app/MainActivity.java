431839705a73f3dd9883c8f083bfe7878ed9c302
fancyburn <fancyburn@protonmail.com>
2026-06-12 12:30:22 +0800

    fix(mobile): register NativeTTS plugin in MainActivity and localize fallback warning

--- android/app/src/main/java/com/edgereader/app/MainActivity.java
+++ android/app/src/main/java/com/edgereader/app/MainActivity.java
@@ -1,5 +1,12 @@
 package com.edgereader.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(NativeTTS.class);
+        super.onCreate(savedInstanceState);
+    }
+}
